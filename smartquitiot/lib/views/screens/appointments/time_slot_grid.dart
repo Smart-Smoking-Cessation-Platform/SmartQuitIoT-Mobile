ca7492f8f5cc997fb17b32a5511acf53ea150469
gitdev-mdat <thiminhdatdaknong@gmail.com>
2025-10-25 17:38:54 +0700

    refactor (appointment) : fix duplicate date into db

--- smartquitiot/lib/views/screens/appointments/time_slot_grid.dart
+++ smartquitiot/lib/views/screens/appointments/time_slot_grid.dart
@@ -1,3 +1,4 @@
+// lib/views/screens/appointments/time_slot_grid.dart
 import 'package:SmartQuitIoT/views/screens/appointments/time_slot_item.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +18,6 @@ class TimeSlotGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // debug: show count
     debugPrint(
       '[DEBUG] TimeSlotGrid.build: count=${timeSlots.length}, selected=$selectedSlot',
     );
@@ -34,16 +34,15 @@ class TimeSlotGrid extends StatelessWidget {
       itemCount: timeSlots.length,
       itemBuilder: (context, index) {
         final slot = timeSlots[index];
-        // defensive: ensure time is non-null
         final slotTime = slot.time ?? '';
         return TimeSlotItem(
           slot: TimeSlot(time: slotTime, available: slot.available),
           isSelected: selectedSlot == slotTime && slotTime.isNotEmpty,
           onTap: slot.available
               ? () {
-                  debugPrint('[DEBUG] TimeSlotGrid tapped: $slotTime');
-                  onSlotSelected(slotTime);
-                }
+            debugPrint('[DEBUG] TimeSlotGrid tapped: $slotTime');
+            onSlotSelected(slotTime);
+          }
               : null,
         );
       },
