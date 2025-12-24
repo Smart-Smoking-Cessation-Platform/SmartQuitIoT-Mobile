81031b4a44cb3e9260253f21e8fa44dc526ea8b0
luan-tran <nkluantran2907@gmail.com>
2025-12-24 14:39:51 +0700

    fix: fix abs

--- smartquitiot/lib/views/screens/stats_table/stats_table_card.dart
+++ smartquitiot/lib/views/screens/stats_table/stats_table_card.dart
@@ -481,6 +481,6 @@ class _StatsTableCardState extends ConsumerState<StatsTableCard>
   String _formatMoney(double amount) {
     // if (amount < 0) return '0';
     final formatter = NumberFormat('#,###');
-    return formatter.format(amount.abs());
+    return formatter.format(amount);
   }
 }
